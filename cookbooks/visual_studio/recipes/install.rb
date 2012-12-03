fb0f9c8f0ec0447b7304a290f1b2242fc548d4b8
Isel Fernandez <isel_77@hotmail.com>
2012-12-03 15:20:53 -0500

    waiting for vs to complete

--- cookbooks/visual_studio/recipes/install.rb
+++ cookbooks/visual_studio/recipes/install.rb
@@ -3,12 +3,6 @@ rightscale_marker :begin
 powershell 'Installing visual studio' do
   script = <<-EOF
     $install_dir = join-path ${ENV:\PROGRAMFILES(X86)} "Microsoft Visual Studio 11.0"
-    if (test-path $install_dir)
-    {
-       Write-Output "VS2012 already installed. Skipping installation."
-       exit 0
-    }
-
     if (test-path "c:\\VS2012\\log.txt")
     {
       Write-Output "VS2012 is in a reboot cycle, let it finish on it's own."
@@ -21,7 +15,15 @@ powershell 'Installing visual studio' do
 
       write-host 'Visual Studio successfully installed'
 
+      rename-item "c:\\VS2012\\log.txt" "c:\\VS2012\\log_complete.txt"
+
       exit 0
+    }
+
+    if (test-path $install_dir)
+    {
+       Write-Output "VS2012 already installed. Skipping installation."
+       exit 0
     }
 
     & "c:\\VS2012\\vs_premium.exe" "/noweb" "/full" "/log" "c:\\VS2012\\log.txt" "/quiet" "/forcerestart"
