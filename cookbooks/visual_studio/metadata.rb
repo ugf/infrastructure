5fccdaad4ad93ea72eef294bcf2eeff4dc5acb33
Isel Fernandez <isel_77@hotmail.com>
2012-12-03 11:23:35 -0500

    adding visual studio cookbook

---
+++ cookbooks/visual_studio/metadata.rb
@@ -0,0 +1,30 @@
+maintainer       "Cloud Infrastructure"
+maintainer_email "csf@ultimatesoftware.com"
+license          "our license"
+description      "Installs visual studio"
+long_description ""
+version          "0.0.1"
+
+supports "windows"
+
+depends 'rightscale'
+depends 'core'
+
+recipe "download", "Downloads visual studio"
+
+attribute "core/aws_access_key_id",
+  :display_name => "aws access key id",
+  :required => "required",
+  :recipes => ["visual_studio::download"]
+
+attribute "core/aws_secret_access_key",
+  :display_name => "aws secret access key",
+  :required => "required",
+  :recipes => ["visual_studio::download"]
+
+attribute "core/s3_bucket",
+  :display_name => "s3 bucket for the UGF platform",
+  :description => "i.e. ugfartifacts, ugfproduction",
+  :required => "optional",
+  :default  => "ugfgate1",
+  :recipes => ["visual_studio::download"]
---
+++ cookbooks/visual_studio/recipes/download.rb
@@ -0,0 +1,25 @@
+rightscale_marker :begin
+
+include_recipe 'core::download_vendor_artifacts_prereqs'
+
+template "#{node[:ruby_scripts_dir]}/download_visual_studio.rb" do
+  local true
+  source "#{node[:ruby_scripts_dir]}/download_vendor_artifacts.erb"
+  variables(
+    :aws_access_key_id => node[:core][:aws_access_key_id],
+    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+    :s3_bucket => node[:core][:s3_bucket],
+    :s3_repository => 'Vendor',
+    :product => 'visualstudio',
+    :version => '2012',
+    :artifacts => 'VS_2012_Premium.zip',
+    :target_directory => '',
+    :unzip => true
+  )
+end
+
+powershell 'downloading visual studio' do
+  source("ruby #{node[:ruby_scripts_dir]}/download_visual_studio.rb")
+end
+
+rightscale_marker :end
