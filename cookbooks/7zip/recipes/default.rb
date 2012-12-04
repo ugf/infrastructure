cb1c266db010ee005bda5c260852240bc8123038
Isel Fernandez <isel_77@hotmail.com>
2012-12-04 14:37:06 -0500

    adding 7zip cookbook

--- cookbooks/7zip/recipes/default.rb
+++ cookbooks/7zip/recipes/default.rb
@@ -8,11 +8,6 @@ cookbook_file "#{installs_directory}/7z465.exe" do
   source '7z465.exe'
 end
 
-windows_package '7zip' do
-  source "#{installs_directory}/7z465.exe"
-  action :install
-end
-
 powershell 'Installing 7zip' do
   source("cmd /c #{installs_directory}/7z465.exe /S")
   not_if { File.exist?("#{ENV['PROGRAMFILES(X86)']}\\7-zip") }
