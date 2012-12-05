72e6b8f5736596134bf39f1f6cb82b32a38a3580
Robert Rehberg <robertrehberg@bellsouth.net>
2012-12-05 17:06:20 -0500

    Removing the unnecessary E drive.

--- cookbooks/teamcity/recipes/web_backup_volumes.rb
+++ cookbooks/teamcity/recipes/web_backup_volumes.rb
@@ -11,18 +11,17 @@ powershell('Backup volumes') do
 
     try
     {
-        if (!(Test-Path "${env:RS_SQLS_DATA_VOLUME}:\") -or !(Test-Path "${env:RS_SQLS_LOGS_VOLUME}:\"))
+        if (!(Test-Path "${env:RS_SQLS_DATA_VOLUME}:\"))
         {
-            Write "Volumes not found - nothing to backup. Exit silently."
+            Write "Volume not found - nothing to backup. Exit silently."
             exit 0
         }
 
         Write-Host "Backup method is Snapshots."
         $dataDevices = $(if ($env:RS_SQLS_DATA_DEVICES) { ([string]$env:RS_SQLS_DATA_DEVICES).Split(',') } else { @() })
-        $logsDevices = $(if ($env:RS_SQLS_LOGS_DEVICES) { ([string]$env:RS_SQLS_LOGS_DEVICES).Split(',') } else { @() })
 
         # using defaults for DB_BACKUP_KEEP_* specified in C:\Program Files (x86)\RightScale\RightLink\sandbox\RightScript\lib\ebs\EbsBackupVolumes.ps1
-        EbsBackupVolumes $env:RS_SQLS_DATA_VOLUME $env:RS_SQLS_LOGS_VOLUME $env:LINEAGE_NAME $dataDevices $logsDevices
+        EbsBackupVolumes $env:RS_SQLS_DATA_VOLUME $env:LINEAGE_NAME $dataDevices
     }
     catch
     {
