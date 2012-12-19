ebed28640a45f6acb714ab2962bda27f339f2891
Robert Rehberg <robertrehberg@bellsouth.net>
2012-12-19 11:43:37 -0500

    Create the directory before trying to write to it

--- cookbooks/iis/recipes/create_deploy_website.rb
+++ cookbooks/iis/recipes/create_deploy_website.rb
@@ -1,5 +1,7 @@
 rightscale_marker :begin
 
+Dir.mkdir(node[:powershell_scripts_dir]) unless File.exist?(node[:powershell_scripts_dir])
+
 template "#{node[:powershell_scripts_dir]}/deploy_website.ps1" do
   source 'deploy_website.erb'
   variables(
