08cc7c44bb0c9b00311861236fa225dcf7e6598f
RonaldO <ronald_ocampo@ultimatesoftware.com>
2012-12-26 13:22:38 -0500

    Removing Testing

--- cookbooks/vagrant/recipes/default.rb
+++ cookbooks/vagrant/recipes/default.rb
@@ -1,7 +1,6 @@
-#TESTING
 
 packages = ['apparmor', 'apt-xapian-index', 'aptitude', 'augeas-lenses', 'debconf-utils', 'installation-report',
-  'language-pack-en-base', 'libbind9-80', 'libexpat1', 'linux-generic-pae', 'linux-headers-generic',
+  'language-pack-en-base', 'libexpat1', 'linux-generic-pae', 'linux-headers-generic',
   'linux-image-generic-pae', 'module-assistant', 'ruby-dev'
 ]
 
