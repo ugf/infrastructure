0a64c4e1a5ee847233e99ec52597913dc62fbd57
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-02 14:38:51 -0500

    fixed passenger installation

--- cookbooks/passenger/recipes/default.rb
+++ cookbooks/passenger/recipes/default.rb
@@ -10,11 +10,16 @@ gem_package 'passenger' do
   options('--no-rdoc --no-ri')
 end
 
-passenger_packages = ['apache2-prefork-dev', 'libapr1-dev', 'libapache2-mod-passenger']
+passenger_packages = ['apache2-prefork-dev', 'libapr1-dev', 'libcurl4-openssl-dev']
 
 passenger_packages.each do |name|
   package name do
   end
+end
+
+execute 'compile passenger' do
+  command './passenger-install-apache2-module --auto'
+  cwd '/opt/ruby/1.9.2-p320/lib/ruby/gems/1.9.1/gems/passenger-3.0.18/bin'
 end
 
 emit_marker :end
