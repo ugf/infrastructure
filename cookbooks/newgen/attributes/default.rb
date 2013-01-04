17ca0dbd85ffc950b5c7c5326cbef1b47dea6d96
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-04 14:03:28 -0500

    326 - Adding download binaries recipe for newgen product

---
+++ cookbooks/newgen/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:ruby_scripts_dir] = '/rubyscripts'
+default[:binaries_directory] = '/deployscripts_binaries'
---
+++ cookbooks/newgen/metadata.rb
@@ -0,0 +1,50 @@
+maintainer       "Cloud Infrastructure"
+maintainer_email "csf@ultimatesoftware.com"
+license          "our license"
+description      "Deploys the UGF software to the environment"
+long_description ""
+version          "0.0.1"
+
+supports "windows"
+
+depends "rightscale"
+
+recipe "newgen::default", "Deploys Infrastructure api services"
+recipe "newgen::download", "Downloads infrastructure api"
+
+# Attributes from core cookbook
+attribute "core/aws_access_key_id",
+  :display_name => "aws access key id",
+  :required => "required",
+  :recipes => ["newgen::download"]
+
+attribute "core/aws_secret_access_key",
+  :display_name => "aws secret access key",
+  :required => "required",
+  :recipes => ["newgen::download"]
+
+attribute "core/s3_bucket",
+  :display_name => "s3 bucket for the UGF platform",
+  :required => "optional",
+  :default  => "ugfgate1",
+  :recipes  => ["newgen::download"]
+
+attribute "core/s3_repository",
+  :display_name => "s3 repository for the services api",
+  :required => "optional",
+  :default  => "NewGen",
+  :recipes  => ["newgen::download"]
+
+# Attributes from newgen cookbook
+attribute "newgen/binaries_artifacts",
+  :display_name => "binaries artifacts",
+  :required => "required",
+  :recipes => ["newgen::download"]
+
+attribute "newgen/binaries_revision",
+  :display_name => "binaries revision",
+  :required => "required",
+  :recipes => ["newgen::download"]
+
+
+
