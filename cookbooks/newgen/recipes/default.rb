8e591d5f8949cabeb51e230b22669ae2e71f30df
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-07 15:27:51 -0500

    326 - refactoring migration again

--- cookbooks/newgen/recipes/default.rb
+++ cookbooks/newgen/recipes/default.rb
@@ -29,8 +29,8 @@ end
 
 execute 'Run migrate' do
   command <<-EOF
-    copy migration\migrate* main_website\bin /y
-    cd main_website\bin
+    copy migration\\migrate* main_website\\bin /y
+    cd main_website\\bin
     migrate.ci.with.username.bat #{node[:newgen][:database_server]} #{node[:newgen][:database_user]} #{node[:newgen][:database_password]}
   EOF
   cwd "#{node[:binaries_directory]}"
