bdd891678559bffdb3863069e594534ea74cdd1a
Isel Fernandez <isel_77@hotmail.com>
2013-01-08 10:31:22 -0500

    319 - running migrate in db server instead of app server

--- cookbooks/newgen/recipes/migrate.rb
+++ cookbooks/newgen/recipes/migrate.rb
@@ -4,16 +4,10 @@ require 'rake'
 
 include_recipe 'newgen::download'
 
-execute 'Adding certificate' do
-  command 'certutil -f -p password -importpfx passiveSTS.pfx'
-  cwd "#{node[:binaries_directory]}/certificate"
-end
-
 ruby_block 'Copying websites' do
   block do
     FileUtils.mkdir_p(node[:websites_directory])
     FileUtils.cp_r("#{node[:binaries_directory]}/main_website", node[:websites_directory])
-    FileUtils.cp_r("#{node[:binaries_directory]}/sts_website", node[:websites_directory])
     FileUtils.cp_r("#{node[:binaries_directory]}/migration/.", "#{node[:websites_directory]}/main_website/bin")
   end
 end
@@ -38,18 +32,9 @@ ruby_block 'Updating config files' do
   end
 end
 
-powershell 'Deploying websites' do
-  parameters({
-    'POWERSHELL_SCRIPTS_DIR' => "c:#{node[:powershell_scripts_dir].gsub('/', '\\')}",
-    'WEBSITES_DIRECTORY' => "c:#{node[:websites_directory].gsub('/', '\\')}"
-  })
-  script = <<-EOF
-    Import-Module "$env:POWERSHELL_SCRIPTS_DIR\\deploy_website.ps1"
-
-    deploy_website 'main website' 'main_website' "$env:WEBSITES_DIRECTORY\\main_website" ':55555:'
-    deploy_website 'sts website' 'sts_website' "$env:WEBSITES_DIRECTORY\\sts_website" ':55556:'
-  EOF
-  source(script)
+execute 'Running migrate' do
+  command "migrate.ci.with.username.bat #{node[:newgen][:database_server]} #{node[:newgen][:database_user]} #{node[:newgen][:database_password]}"
+  cwd "#{node[:websites_directory]}/main_website/bin"
 end
 
 rightscale_marker :end
