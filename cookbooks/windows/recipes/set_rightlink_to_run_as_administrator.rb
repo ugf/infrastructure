4a65176f4ba800bd6f8b0c27f2d4d9511fe6f7da
Isel Fernandez <isel_77@hotmail.com>
2013-01-10 17:13:31 -0500

    240 - should only set the account, when bundled, the service will start correctly

--- cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
+++ cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
@@ -1,12 +1,7 @@
 rightscale_marker :begin
 
-powershell 'Setting RightLink login account' do
-  parameters({ 'password' => node[:windows][:administrator_password] })
-  script = <<-EOF
-    sc config RightLink obj= administrator password= $env:password
-    Restart-Service RightLink
-  EOF
-  source(script)
+execute 'Setting RightLink login account' do
+  command "sc config RightLink obj= administrator password= #{node[:windows][:administrator_password]}"
 end
 
 rightscale_marker :end
