ba084b811d52332ad9b11def20886d9fa451f3c8
Isel Fernandez <isel_77@hotmail.com>
2013-01-10 17:06:04 -0500

    240 - adding recipe to set rightlink to run as administrator

--- cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
+++ cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
@@ -1,7 +1,12 @@
 rightscale_marker :begin
 
-execute 'Setting RightLink login account' do
-  command "sc config RightLink obj= administrator password= #{node[:windows][:administrator_password]}"
+powershell 'Setting RightLink login account' do
+  parameters({ 'password' => node[:windows][:administrator_password] })
+  script = <<-EOF
+    sc config RightLink obj= administrator password= $env:password
+    Restart-Service RightLink
+  EOF
+  source(script)
 end
 
 rightscale_marker :end
