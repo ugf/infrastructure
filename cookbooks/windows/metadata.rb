5bbc7f1bba848899f71c8308f2580339fc976e8a
Isel Fernandez <isel_77@hotmail.com>
2013-01-10 16:54:41 -0500

    240 - adding recipe to set rightlink to run as administrator

--- cookbooks/windows/metadata.rb
+++ cookbooks/windows/metadata.rb
@@ -9,8 +9,9 @@ supports "windows"
 
 recipe "windows::assign_logon_as_a_service_to_administrator", "assign logon as a service to administrator"
 recipe "windows::set_administrator_password", "sets the administrator password"
+recipe "windows::set_rightlink_to_run_as_administrator", "sets the the rightlink service to run as administrator"
 
 attribute "windows/administrator_password",
   :display_name => "administrator password",
   :required => "required",
-  :recipes => ["windows::set_administrator_password"]
+  :recipes => ["windows::set_administrator_password", "windows::set_rightlink_to_run_as_administrator"]
