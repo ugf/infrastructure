1b1c634d7efa10ad06aeb91e5d22db432ccce7ad
mike suarez <mikemps@gmail.com>
2013-01-11 13:27:03 -0500

    240 - rebooting to restart service

--- cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
+++ cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
@@ -1,11 +1,11 @@
 rightscale_marker :begin
 
-execute 'Setting RightLink login account' do
-  command "sc config RightLink obj= .\\administrator password= #{node[:windows][:administrator_password]}"
-end
-
 execute 'Reboot' do
   command 'sc qc RightLink | find "SERVICE_START_NAME : .\administrator" || shutdown /r'
+end
+
+execute 'Setting RightLink login account' do
+  command "sc config RightLink obj= .\\administrator password= #{node[:windows][:administrator_password]}"
 end
 
 rightscale_marker :end
