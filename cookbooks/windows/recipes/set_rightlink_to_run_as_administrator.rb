0b28fd489c75e383833d05343071c77fb916d0a5
Isel Fernandez <isel_77@hotmail.com>
2013-01-10 20:21:15 -0500

    240 - fixing user name

--- cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
+++ cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
@@ -1,7 +1,7 @@
 rightscale_marker :begin
 
 execute 'Setting RightLink login account' do
-  command "sc config RightLink obj= administrator password= #{node[:windows][:administrator_password]}"
+  command "sc config RightLink obj= .\\administrator password= #{node[:windows][:administrator_password]}"
 end
 
 rightscale_marker :end
