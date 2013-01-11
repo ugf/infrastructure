63d2b129e974305a3805f1d0579e7afad4c7ddd3
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-11 10:03:24 -0500

    240 - Reboot after assigning rightlink to run as administrator

--- cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
+++ cookbooks/windows/recipes/set_rightlink_to_run_as_administrator.rb
@@ -1,7 +1,12 @@
 rightscale_marker :begin
 
+windows_reboot 60 do
+  action :nothing
+end
+
 execute 'Setting RightLink login account' do
   command "sc config RightLink obj= .\\administrator password= #{node[:windows][:administrator_password]}"
+  notifies :request, 'windows_reboot[60]'
 end
 
 rightscale_marker :end
