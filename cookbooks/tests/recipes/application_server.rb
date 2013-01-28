a1c8ba914a8ec28e8d51f69bf60f55ab62f3b54a
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-28 14:12:11 -0500

    Checking cache path

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -1,9 +1,11 @@
-template 'run_feature_for.rb' do
-  source 'scripts/run_feature_for.rb.erb'
-  variables(:tags => '@application_server')
-end
+#template 'run_feature_for.rb' do
+#  source 'scripts/run_feature_for.rb.erb'
+#  variables(:tags => '@application_server')
+#end
+#
+#powershell('Running tests') do
+#  source('ruby run_feature_for.rb')
+#end
 
-powershell('Running tests') do
-  source('ruby run_feature_for.rb')
-end
+puts file_cache_path
 
