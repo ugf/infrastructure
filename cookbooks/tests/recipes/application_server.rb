8df03aaa82100337187fffcc2c7836914de48d9e
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-28 15:07:55 -0500

    351 - Adding cucumber feature structure to run tests

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -1,11 +1,25 @@
-#template 'run_feature_for.rb' do
-#  source 'scripts/run_feature_for.rb.erb'
-#  variables(:tags => '@application_server')
-#end
-#
-#powershell('Running tests') do
-#  source('ruby run_feature_for.rb')
-#end
 
-log "Cache path: #{Chef::Config[:file_cache_path]}"
+log "Cache path: #{p Chef::Config}"
 
+features_directory = node[:features_directory]
+
+Dir.mkdir(features_directory) unless File.exist?(features_directory)
+
+template "#{features_directory}/mstest.feature" do
+  source 'features/mstest.feature.erb'
+end
+
+Dir.mkdir("#{features_directory}/step_definitions") unless File.exist?("#{features_directory}/step_definitions")
+
+template "#{features_directory}/step_definitions/file_system.rb" do
+  source 'features/step_definitions/file_system.erb'
+end
+
+template "#{features_directory}/run_feature_for.rb" do
+  source 'scripts/run_feature_for.rb.erb'
+  variables(:tags => '@application_server')
+end
+
+powershell('Running tests') do
+  source("ruby #{features_directory}/run_feature_for.rb")
+end
