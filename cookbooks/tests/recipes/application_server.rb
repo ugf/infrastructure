d8afa2ea52bd7780d27dc85f39343a5d7c4a0f9d
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-28 16:14:22 -0500

    351 - Adding mstest test

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -1,7 +1,4 @@
 
-log "Keys: #{puts Chef::Config.keys}"
-log "Values: #{puts Chef::Config.values}"
-
 features_directory = node[:features_directory]
 
 Dir.mkdir(features_directory) unless File.exist?(features_directory)
@@ -20,16 +17,3 @@ execute 'Running tests' do
   command "cucumber #{features_directory}"
   cwd '/'
 end
-
-#template "#{node[:ruby_scripts_dir]}/run_feature_for.rb" do
-#  source 'scripts/run_feature_for.rb.erb'
-#  variables(
-#    :features_directory => features_directory,
-#    :tags => '@application_server'
-#  )
-#end
-
-
-#powershell('Running tests') do
-#  source("ruby #{node[:ruby_scripts_dir]}/run_feature_for.rb")
-#end
