531633a47364c74042830eabd1e95809d9020ea0
RonaldO <ronald_ocampo@ultimatesoftware.com>
2013-01-28 15:54:46 -0500

    351 - Running cucumber tests from cmd

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -16,14 +16,20 @@ template "#{features_directory}/step_definitions/file_system.rb" do
   source 'features/step_definitions/file_system.rb.erb'
 end
 
-template "#{node[:ruby_scripts_dir]}/run_feature_for.rb" do
-  source 'scripts/run_feature_for.rb.erb'
-  variables(
-    :features_directory => features_directory,
-    :tags => '@application_server'
-  )
+execute 'Running tests' do
+  command "cucumber #{features_directory}"
+  cwd '/'
 end
 
-powershell('Running tests') do
-  source("ruby #{node[:ruby_scripts_dir]}/run_feature_for.rb")
-end
+#template "#{node[:ruby_scripts_dir]}/run_feature_for.rb" do
+#  source 'scripts/run_feature_for.rb.erb'
+#  variables(
+#    :features_directory => features_directory,
+#    :tags => '@application_server'
+#  )
+#end
+
+
+#powershell('Running tests') do
+#  source("ruby #{node[:ruby_scripts_dir]}/run_feature_for.rb")
+#end
