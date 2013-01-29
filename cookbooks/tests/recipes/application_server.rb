08e9cad902307b7a5b22675c909cf1bf8b5903fd
Isel Fernandez <isel_77@hotmail.com>
2013-01-29 13:16:33 -0500

    351 - playing with deploy resource

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -1,7 +1,26 @@
+rightscale_marker :begin
 
 features_directory = node[:features_directory]
+repo_directory = '/tests_repository'
 
 Dir.mkdir(features_directory) unless File.exist?(features_directory)
+
+Dir.mkdir(repo_directory) unless File.exist?(repo_directory)
+
+deploy repo_directory do
+  repo 'git://github.com/ugf/chef_examples.git'
+  revision 'HEAD' # or "HEAD" or "TAG_for_1.0" or (subversion) "1234"
+  user 'root'
+  #enable_submodules true
+  #migrate true
+  #migration_command "rake db:migrate"
+  #environment "RAILS_ENV" => "production", "OTHER_ENV" => "foo"
+  #shallow_clone true
+  action :deploy # or :rollback
+  #restart_command "touch tmp/restart.txt"
+  #git_ssh_wrapper "wrap-ssh4git.sh"
+  scm_provider Chef::Provider::Git # is the default, for svn: Chef::Provider::Subversion
+end
 
 template "#{features_directory}/mstest.feature" do
   source 'features/mstest.feature.erb'
@@ -17,3 +36,5 @@ execute 'Running tests' do
   command "cucumber #{features_directory}"
   cwd '/'
 end
+
+rightscale_marker :end
