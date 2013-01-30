9a261d0a279f7c0d22d091c9ec1b6e37b89936af
Isel Fernandez <isel_77@hotmail.com>
2013-01-30 10:49:35 -0500

    351 - removing previous clone of tests

--- cookbooks/tests/recipes/default.rb
+++ cookbooks/tests/recipes/default.rb
@@ -1,6 +1,12 @@
 rightscale_marker :begin
 
 git = "\"#{ENV['PROGRAMFILES(X86)']}\\Git\\bin\\git\""
+tests_directory = '/infrastructure_tests'
+
+execute 'Removing previous clone' do
+  command "rd #{tests_directory} /s /q"
+  only_if { File.exist?(tests_directory) }
+end
 
 execute 'Downloading tests' do
   command "#{git} clone git://github.com/ugf/infrastructure_tests.git"
@@ -9,7 +15,7 @@ end
 
 execute 'Checkout tests revision' do
   command "#{git} checkout #{node[:tests][:revision]}"
-  cwd '/infrastructure_tests'
+  cwd tests_directory
   not_if { node[:tests][:revision] == 'head' }
 end
 
