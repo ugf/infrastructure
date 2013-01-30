868a5d0c64b19765de233c5484125a172b155519
Isel Fernandez <isel_77@hotmail.com>
2013-01-30 09:50:27 -0500

    351 - specifying tests revision and running tests based on category

--- cookbooks/tests/recipes/default.rb
+++ cookbooks/tests/recipes/default.rb
@@ -1,7 +1,11 @@
 rightscale_marker :begin
 
 execute 'Downloading tests' do
-  command "\"#{ENV['PROGRAMFILES(X86)']}\\Git\\bin\\git\" clone git://github.com/ugf/infrastructure_tests.git"
+  command <<-EOF
+    "#{ENV['PROGRAMFILES(X86)']}\\Git\\bin\\git" clone git://github.com/ugf/infrastructure_tests.git
+    cd 'infrastructure_tests'
+    git checkout #{node[:tests][:revision]}
+EOF
   cwd '/'
 end
 
