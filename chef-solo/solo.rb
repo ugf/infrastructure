c4570b6888461d9bef334f3f390868108e089a79
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-31 13:54:18 -0500

    366 - change the order of the repositories for monkey patching

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,3 +1,3 @@
 file_cache_path "/all_cookbooks/chef-solo"
-cookbook_path ["/all_cookbooks/infrastructure/cookbooks", "/all_cookbooks/powershell/cookbooks"]
+cookbook_path ["/all_cookbooks/powershell/cookbooks", "/all_cookbooks/infrastructure/cookbooks"]
 json_attribs "/all_cookbooks/infrastructure/chef-solo/windows_build_server.json"
