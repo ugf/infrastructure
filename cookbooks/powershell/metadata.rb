5684f3bccf193287c041d4d1613f53355892ea67
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-31 16:05:47 -0500

    366 - monkey patching opscode version of powershell to be rightscale compatible

--- cookbooks/powershell/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer "Cloud Infrastructure"
-maintainer_email "csf@ultimatesoftware.com"
-license "our license"
-description "Monkey-patching the opscode powershell provider"
-long_description ""
-version "0.0.1"
-
-recipe "powershell::default", "Monkey patches the opscode provider"
