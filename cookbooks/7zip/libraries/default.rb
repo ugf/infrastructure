c2b991106c16435fd6b9375c06018a6d37b9924c
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-31 16:21:11 -0500

    366 - monkey patching opscode version of powershell to be rightscale compatible

--- cookbooks/7zip/libraries/default.rb
+++ cookbooks/7zip/libraries/default.rb
@@ -1,7 +1,6 @@
-
 Chef::Log.info("I am in patch")
 
-Chef::Log.info( Chef::Resource::Powershell.name )
+Chef::Log.info(Chef::Resource::Powershell.name)
 class Chef::Resource::Powershell
   def source(arg)
     code(arg)
