b0ee801f9228dc2265457bb209d988f3f826861a
Robert Rehberg <robertrehberg@bellsouth.net>
2013-01-31 10:15:29 -0500

    317 - refactored the tests

--- spec/jdk_spec.rb
+++ spec/jdk_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
-main = self
-
 describe 'jdk' do
 
   before :each do
-    stub(main).rightscale_marker
-    stub(main).include_recipe
-    stub(main).template
+    stub_the.rightscale_marker
+    stub_the.include_recipe
+    stub_the.template
   end
 
   context 'When the platform is windows' do
@@ -15,27 +13,27 @@ describe 'jdk' do
     let(:artifact) { 'jdk_windows' }
 
     before :each do
-      stub(main).powershell
-      stub(main).windows_zipfile
-      stub(main).node { {platform: 'windows', ruby_scripts_dir: '/ruby192' } }
-      stub(main).source
+      stub_the.powershell
+      stub_the.windows_zipfile
+      stub_the.node { {platform: 'windows', ruby_scripts_dir: '/ruby192' } }
+      stub_the.source
       stub(File).exist?
-      stub(main).env
+      stub_the.env
     end
 
     it 'the installer is downloaded' do
-      stub(main).not_if
-      stub(main).powershell.yields
+      stub_the.not_if
+      stub_the.powershell.yields
 
-      mock(main).source 'ruby /ruby192/download_jdk.rb'
+      mock_the.source 'ruby /ruby192/download_jdk.rb'
 
       run_recipe 'jdk'
     end
 
     it 'the installer is not download twice' do
-      stub(main).source
-      stub(main).not_if.yields
-      stub(main).powershell.yields
+      stub_the.source
+      stub_the.not_if.yields
+      stub_the.powershell.yields
 
       mock(File).exist?("#{download_directory}/#{artifact}.zip") { true }
 
@@ -43,24 +41,24 @@ describe 'jdk' do
     end
 
     it 'the installer is unzipped' do
-      stub(main).not_if
-      stub(main).action
+      stub_the.not_if
+      stub_the.action
 
-      mock(main).windows_zipfile("#{download_directory}/#{artifact}").yields
-      mock(main).source("#{download_directory}/#{artifact}.zip")
+      mock_the.windows_zipfile("#{download_directory}/#{artifact}").yields
+      mock_the.source("#{download_directory}/#{artifact}.zip")
 
       run_recipe 'jdk'
     end
 
     it 'sets environment variables to the install directory' do
-      stub(main).action
-      stub(main).delim
+      stub_the.action
+      stub_the.delim
 
-      mock(main).env('JAVA_HOME').yields
-      mock(main).env('JRE_HOME').yields
-      mock(main).env('PATH').yields
+      mock_the.env('JAVA_HOME').yields
+      mock_the.env('JRE_HOME').yields
+      mock_the.env('PATH').yields
 
-      mock(main).value('c:\jdk\bin').times(3)
+      mock_the.value('c:\jdk\bin').times(3)
 
       run_recipe 'jdk'
     end
