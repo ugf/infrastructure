afa30e59ba8c3337b1cff0838415da6c4041520e
Robert Rehberg <robertrehberg@bellsouth.net>
2013-02-01 14:38:32 -0500

    366 - download from denver2 instead of s3

--- cookbooks/visual_studio/recipes/default.rb
+++ cookbooks/visual_studio/recipes/default.rb
@@ -1,4 +1,12 @@
-rightscale_marker :begin
+class Chef::Resource
+  include DetectVagrant
+end
+
+class Chef::Recipe
+  include DetectVagrant
+end
+
+emit_marker :begin
 
 Dir.mkdir(node[:ruby_scripts_dir]) unless File.exist? node[:ruby_scripts_dir]
 
@@ -6,7 +14,7 @@ cookbook_file "#{node[:ruby_scripts_dir]}/UtilityFunctions.ps1" do
   source 'UtilityFunctions.ps1'
 end
 
-powershell 'downloading visual studio' do
+powershell 'downloading visual studio via s3' do
   parameters(
     {
       'AWS_ACCESS_KEY_ID' => node[:core][:aws_access_key_id],
@@ -66,35 +74,50 @@ powershell 'downloading visual studio' do
     cmd /c "${env:\ProgramFiles(x86)}\\7-Zip\\7z.exe" x -y -oc:\\vs2012 -r "c:\\installs\\VS_2012_Premium.zip"
   EOF
   source(script)
+  not_if { node[:core][:aws_access_key_id].empty? || node[:core][:aws_secret_access_key].empty? }
 end
 
-powershell 'Installing visual studio' do
-  script = <<-EOF
-    $install_dir = join-path ${ENV:\PROGRAMFILES(X86)} "Microsoft Visual Studio 11.0"
-    if (test-path "c:\\VS2012\\log.txt")
-    {
-      Write-Output "VS2012 is in a reboot cycle, let it finish on it's own."
+ruby_block 'downloading visual studio via denver2' do
+  block do
+    Dir.mkdir('c:\installs') unless File.exist?('c:\installs')
 
-      do {
-        write-host 'Waiting for visual studio...'
-        start-sleep 5
-      }
-      until ((Get-Content c:\\vs2012\\log.txt | Select-Object -last 1).Contains('Exit code: 0x0, restarting: No'))
+    `net use \\\\denver2\groups /user:devcorp\\\\svc.tv teamv`
 
-      write-host 'Visual Studio successfully installed'
+    `xcopy \\\\denver2\\groups\\Build and Deployment\\newgen\\repository\\ugfinfrastructure\\VS_2012_Premium.zip c:\\installs`
 
-      exit 0
-    }
+    `net use \\\\denver2\\groups /delete`
 
-    if (test-path $install_dir)
-    {
-       Write-Output "VS2012 already installed. Skipping installation."
-       exit 0
-    }
-
-    & "c:\\VS2012\\vs_premium.exe" "/noweb" "/full" "/log" "c:\\VS2012\\log.txt" "/quiet" "/forcerestart"
-  EOF
-  source(script)
+  end
+  only_if { node[:core][:aws_access_key_id].empty? && node[:core][:aws_secret_access_key].empty? }
 end
 
-rightscale_marker :end
+#powershell 'Installing visual studio' do
+#  script = <<-EOF
+#    $install_dir = join-path ${ENV:\PROGRAMFILES(X86)} "Microsoft Visual Studio 11.0"
+#    if (test-path "c:\\VS2012\\log.txt")
+#    {
+#      Write-Output "VS2012 is in a reboot cycle, let it finish on it's own."
+#
+#      do {
+#        write-host 'Waiting for visual studio...'
+#        start-sleep 5
+#      }
+#      until ((Get-Content c:\\vs2012\\log.txt | Select-Object -last 1).Contains('Exit code: 0x0, restarting: No'))
+#
+#      write-host 'Visual Studio successfully installed'
+#
+#      exit 0
+#    }
+#
+#    if (test-path $install_dir)
+#    {
+#       Write-Output "VS2012 already installed. Skipping installation."
+#       exit 0
+#    }
+#
+#    & "c:\\VS2012\\vs_premium.exe" "/noweb" "/full" "/log" "c:\\VS2012\\log.txt" "/quiet" "/forcerestart"
+#  EOF
+#  source(script)
+#end
+
+emit_marker :end
