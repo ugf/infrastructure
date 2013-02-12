3b4a837edaee82ca0bbef7aee9789d9406aaebc5
Isel Fernandez <isel_77@hotmail.com>
2013-02-12 11:06:03 -0500

    372 - fixing attributes

--- cookbooks/elmah/metadata.rb
+++ cookbooks/elmah/metadata.rb
@@ -10,12 +10,12 @@ depends 'windows'
 
 recipe 'elmah::default', 'Sets up elmah database'
 
-attribute 'teamcity/database_user',
+attribute 'elmah/database_user',
   :display_name => 'database user',
   :required => 'required',
   :recipes => ['elmah::default']
 
-attribute 'teamcity/database_password',
+attribute 'elmah/database_password',
   :display_name => 'database password',
   :required => 'required',
   :recipes => ['elmah::default']
