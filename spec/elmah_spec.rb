129226586499773eb655276448708e33adba9bc1
mike suarez <mikemps@gmail.com>
2013-02-12 11:41:42 -0500

    372 - done with elmah unit tests

--- spec/elmah_spec.rb
+++ spec/elmah_spec.rb
@@ -4,6 +4,8 @@ describe 'elmah' do
 
   let(:create_db_script) { 'create_database.sql' }
   let(:create_db_script_path) { "c:\\#{create_db_script}" }
+  let(:setup_db_script) { 'setup_database.sql' }
+  let(:setup_db_script_path) { "c:\\#{setup_db_script}" }
 
   before :each do
     stub_all
@@ -32,6 +34,24 @@ describe 'elmah' do
   end
 
   it 'should generate the db' do
+
+    stub_the.execute('Create elmah database').yields
+    mock_the.command "sqlcmd -E -i#{create_db_script_path}"
+
+  end
+
+  it 'should generate setup db script' do
+
+    stub_the.template(setup_db_script_path).yields
+    mock_the.source "#{setup_db_script}.erb"
+
+  end
+
+  it 'should setup the db' do
+
+    stub_the.execute('Setup elmah database').yields
+    mock_the.command "sqlcmd -E -i#{setup_db_script_path}"
+
   end
 
 end
