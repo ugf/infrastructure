84b228b6f51f4e8ba298b304dd9288e78d1610e0
mike suarez <mikemps@gmail.com>
2013-02-13 15:58:32 -0500

    372 - adding share temp recipe

---
+++ cookbooks/windows/recipes/share_temp_folder.rb
@@ -0,0 +1,15 @@
+rightscale_marker :begin
+
+DIR = 'c:\temp'
+
+execute 'Create temp folder' do
+  command "mkdir #{DIR}"
+  not_if { Dir.exists? DIR }
+end
+
+execute 'Share temp folder' do
+  command "net share temp=#{DIR}"
+  only_if { `net share temp | find "temp"`.empty? }
+end
+
+rightscale_marker :end
