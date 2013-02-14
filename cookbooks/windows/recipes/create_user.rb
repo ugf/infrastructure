d15394e4eab3b7babf57aeae07a1baee38137b88
mike suarez <mikemps@gmail.com>
2013-02-14 11:59:16 -0500

    377 - undo missing fo' now

--- cookbooks/windows/recipes/create_user.rb
+++ cookbooks/windows/recipes/create_user.rb
@@ -3,13 +3,9 @@ rightscale_marker :begin
 user = node[:windows][:new_user_name]
 pass = node[:windows][:new_user_password]
 
-def missing?(cmd, txt)
-  `#{cmd} | find "#{txt}"`.empty?
-end
-
 execute "Create #{user} user" do
   command "net user #{user} #{pass} /add /expires:never"
-  only_if { missing? "net user #{user}", user }
+  only_if { `net user #{user} | find "#{user}"`.empty? }
 end
 
 rightscale_marker :end
--- cookbooks/windows/recipes/share_temp_folder.rb
+++ cookbooks/windows/recipes/share_temp_folder.rb
@@ -2,9 +2,9 @@ rightscale_marker :begin
 
 DIR = 'c:\temp'
 
-def missing?(cmd, txt)
-  `#{cmd} | find "#{txt}"`.empty?
-end
+#def missing?(cmd, txt)
+#  `#{cmd} | find "#{txt}"`.empty?
+#end
 
 execute 'Create temp folder' do
   command "mkdir #{DIR}"
@@ -13,7 +13,7 @@ end
 
 execute 'Share temp folder' do
   command "net share temp=#{DIR} /GRANT:Everyone,READ"
-  only_if { missing? 'net share temp', 'temp' }
+  only_if { `net share temp | find "temp"`.empty? }
 end
 
 rightscale_marker :end
