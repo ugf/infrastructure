83cb074ccec401745c7fb189fb3342dd163a5aca
mike suarez <mikemps@gmail.com>
2013-02-14 10:01:50 -0500

    377 - adding read for all

--- cookbooks/windows/recipes/share_temp_folder.rb
+++ cookbooks/windows/recipes/share_temp_folder.rb
@@ -8,7 +8,7 @@ execute 'Create temp folder' do
 end
 
 execute 'Share temp folder' do
-  command "net share temp=#{DIR}"
+  command "net share temp=#{DIR} /GRANT:Everyone,READ"
   only_if { `net share temp | find "temp"`.empty? }
 end
 
