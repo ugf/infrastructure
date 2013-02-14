9176d70feae0b9c9a1f7a9efb9d1bed7c2904b1d
mike suarez <mikemps@gmail.com>
2013-02-14 11:24:48 -0500

    377 - extracted missing?

--- cookbooks/windows/recipes/create_user.rb
+++ cookbooks/windows/recipes/create_user.rb
@@ -3,10 +3,13 @@ rightscale_marker :begin
 user = node[:windows][:new_user_name]
 pass = node[:windows][:new_user_password]
 
+def missing?(cmd, txt)
+  `#{cmd} | find "#{txt}"`.empty?
+end
+
 execute "Create #{user} user" do
   command "net user #{user} #{pass} /add /expires:never"
-  #only_if "`net user #{user} | find \"#{user}\"`.empty?"
-  only_if { `net user #{user} | find "#{user}"`.empty? }
+  only_if { missing? "net user #{user}", user }
 end
 
 rightscale_marker :end
--- cookbooks/windows/recipes/share_temp_folder.rb
+++ cookbooks/windows/recipes/share_temp_folder.rb
@@ -2,6 +2,10 @@ rightscale_marker :begin
 
 DIR = 'c:\temp'
 
+def missing?(cmd, txt)
+  `#{cmd} | find "#{txt}"`.empty?
+end
+
 execute 'Create temp folder' do
   command "mkdir #{DIR}"
   not_if { File.exist? DIR }
@@ -9,7 +13,7 @@ end
 
 execute 'Share temp folder' do
   command "net share temp=#{DIR} /GRANT:Everyone,READ"
-  only_if { `net share temp | find "temp"`.empty? }
+  only_if { missing? 'net share temp', 'temp' }
 end
 
 rightscale_marker :end
