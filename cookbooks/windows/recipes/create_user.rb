35600e5a6d0321332ab58b63a6a5177eb74b7015
mike suarez <mikemps@gmail.com>
2013-02-14 11:15:56 -0500

    377 - small bug

--- cookbooks/windows/recipes/create_user.rb
+++ cookbooks/windows/recipes/create_user.rb
@@ -5,7 +5,8 @@ pass = node[:windows][:new_user_password]
 
 execute "Create #{user} user" do
   command "net user #{user} #{pass} /add /expires:never"
-  only_if "`net user #{user} | find \"#{user}\"`.empty?"
+  #only_if "`net user #{user} | find \"#{user}\"`.empty?"
+  only_if { `net user #{user} | find "#{user}"`.empty? }
 end
 
 rightscale_marker :end
