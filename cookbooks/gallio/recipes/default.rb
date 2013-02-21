9fdc822184a6cba7967b8ad2762daaa2e5abe550
Ronald Ocampo <ronald_ocampo@ultimatesoftware.com>
2013-02-21 18:06:50 -0500

    382 - Adding gallio recipe and corresponding spec tests

---
+++ cookbooks/gallio/recipes/default.rb
@@ -0,0 +1,36 @@
+rightscale_marker :begin
+
+include_recipe 'core::download_vendor_artifacts_prereqs'
+
+raise 'Not implemented yet' if node[:platform] == 'ubuntu'
+
+artifact = 'gallio'
+
+template "#{node[:ruby_scripts_dir]}/download_gallio.rb" do
+  local true
+  source "#{node[:ruby_scripts_dir]}/download_vendor_artifacts.erb"
+  variables(
+    :aws_access_key_id => node[:core][:aws_access_key_id],
+    :aws_secret_access_key => node[:core][:aws_secret_access_key],
+    :repository_source => node[:core][:repository_source],
+    :s3_bucket => node[:core][:s3_bucket],
+    :s3_repository => 'Vendor',
+    :product => artifact,
+    :version => '3.4.14.0',
+    :artifacts => artifact,
+    :target_directory => node[:installs_directory]
+  )
+end
+
+execute 'Download gallio' do
+  command "ruby #{node[:ruby_scripts_dir]}/download_gallio.rb"
+  not_if { File.exist?("#{node[:installs_directory]}/#{artifact}.zip") }
+end
+
+windows_zipfile "#{node[:installs_directory]}/#{artifact}" do
+  source "#{node[:installs_directory]}/#{artifact}.zip"
+  action :unzip
+  not_if { File.exist?("#{node[:installs_directory]}/#{artifact}") }
+end
+
+rightscale_marker :end
---
+++ spec/gallio_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'Gallio' do
+  let(:ruby_scripts_dir) { '/rubyscripts' }
+  let(:installs_directory) { '/installs' }
+  let(:artifact) { 'gallio' }
+
+  before :each do
+    stub_all
+    given.node {{platform: 'windows'}}
+  end
+
+  describe 'default' do
+    after :each do
+      run_recipe 'gallio'
+    end
+
+    it 'should include download_vendor_artifacts_prereqs recipe' do
+      verify.include_recipe 'core::download_vendor_artifacts_prereqs'
+    end
+
+    it 'should raise error for ubuntu' do
+      given.node {{platform: 'ubuntu'}}
+      verify.raise 'Not implemented yet'
+    end
+
+    it 'should create download script' do
+      given.node {{ruby_scripts_dir: ruby_scripts_dir}}
+      verify.template "#{ruby_scripts_dir}/download_gallio.rb"
+    end
+
+    it 'should download gallio' do
+      given.node {{ruby_scripts_dir: ruby_scripts_dir}}
+      given.execute('Download gallio').yields
+      verify.command "ruby #{ruby_scripts_dir}/download_gallio.rb"
+    end
+
+    it 'should unzip artifact' do
+      given.node {{installs_directory: installs_directory}}
+      given.windows_zipfile("#{installs_directory}/#{artifact}").yields
+      verify.source "#{installs_directory}/#{artifact}.zip"
+      verify.action :unzip
+    end
+  end
+end
